ca44eee58faa8da8c03e7d745c0e79aa6dd91b5b
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-01 21:08:02 +0530

    feat: add action to get current user

---
+++ src/modules/authentication/actions/index.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { db } from '@/db';
+import { user } from '@/db/schema';
+import { auth } from '@/lib/auth';
+import { eq } from 'drizzle-orm';
+import { headers } from 'next/headers';
+
+export async function getCurrentUser() {
+  try {
+    const session = await auth.api.getSession({ headers: await headers() });
+
+    if (!session?.user?.id) {
+      return null;
+    }
+
+    const currenrUser = await db
+      .select({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        image: user.image,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      })
+      .from(user)
+      .where(eq(user.id, session.user.id))
+      .limit(1);
+
+    return currenrUser[0] ?? null;
+  } catch (error) {
+    console.error('Error fetching current user:', error);
+    return null;
+  }
+}

589ec4ca88d06dad268dac184bc9299b5418e976
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-18 23:34:22 +0530

    learn strings in rust

--- src/lessons/strings.rs
+++ src/lessons/strings.rs
@@ -1,8 +1,16 @@
 pub fn strings() {
     println!("Strings");
-    let phrase: String = String::from("This is a beautiful picture");
+    let phrase = String::from("This is a beautiful picture");
 
-    println!("{}", phrase);
+    println!("{phrase}");
+
+    let index: usize = 10;
+    let character_pos = phrase.chars().nth(index);
+
+    match character_pos {
+        Some(c) => println!("Character at index {index} is '{c}'"),
+        None => println!("No character at index {index}"),
+    }
 
     println!();
 }

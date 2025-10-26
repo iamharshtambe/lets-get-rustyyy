9a003794b5ce0ab168261ff7dbece08a9ebaf519
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-26 21:35:05 +0530

    split user name into first and last name

--- 01 - Functions, Variables /main.py
+++ 01 - Functions, Variables /main.py
@@ -6,5 +6,8 @@ name = (
     input("What is your name? ").strip().title()
 )  # remove whitespaces and capitalize first letter
 
+# split user's name into first and last name
+first_name, last_name = name.split(" ")
+
 # say hello to the user
-print(f"Hello, {name}")
+print(f"Hello, {first_name}")

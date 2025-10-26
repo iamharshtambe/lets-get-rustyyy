ca7f5ed269156b6d4f996aab6b28df1725a0bfec
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-26 22:29:29 +0530

    make calculator v4 with add sub mul and div

--- 01 - Functions, Variables /calculator.py
+++ 01 - Functions, Variables /calculator.py
@@ -2,3 +2,9 @@ x = float(input("Enter first number: "))
 y = float(input("Enter second number: "))
 
 print(f"Addition of {x} + {y} = {round(x + y)}")
+
+print(f"Subtraction of {x} - {y} = {round(x - y)}")
+
+print(f"Multiplication of {x} * {y} = {round(x * y)}")
+
+print(f"Division of {x} / {y} = {round(x / y, ndigits=2)}")

fbe9d3d733754b770d9bda8596e2bd018f62f554
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-27 14:55:48 +0530

    use custom functions

--- 01 - Functions, Variables /main.py
+++ 01 - Functions, Variables /main.py
@@ -1,13 +1,16 @@
 # my first python program
 print("Hello, World!")
 
+
+def say_hello(name="User"):
+    print(f"Hello, {name}")
+
+
 # ask user for their name
 name = (
     input("What is your name? ").strip().title()
 )  # remove whitespaces and capitalize first letter
 
-# split user's name into first and last name
-first_name, last_name = name.split(" ")
-
 # say hello to the user
-print(f"Hello, {first_name}")
+say_hello(name)
+say_hello()

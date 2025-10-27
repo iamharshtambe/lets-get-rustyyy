7b5e43d3aac51f31d11086d20765da255967e8ec
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-27 21:17:11 +0530

    calculate grade according to score v1

--- 02 - Conditionals/main.py
+++ 02 - Conditionals/main.py
@@ -11,7 +11,22 @@ else:
 a = int(input("Enter A = "))
 b = int(input("Enter B = "))
 
+# equal or not
 if a != b:
     print("A and B are not equal")
 else:
     print("A and B are equal")
+
+# grade according to score
+score = int(input("Enter your score: "))
+
+if score >= 90 and score <= 100:
+    print("Grade A")
+elif score >= 80 and score < 90:
+    print("Grade B")
+elif score >= 70 and score < 80:
+    print("Grade C")
+elif score >= 60 and score < 70:
+    print("Grade D")
+else:
+    print("Go and study! You have failed...")

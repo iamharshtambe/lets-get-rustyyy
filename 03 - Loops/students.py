81ef7d532db245feee210180535684aded328e42
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-28 20:52:51 +0530

    students v2

--- 03 - Loops/students.py
+++ 03 - Loops/students.py
@@ -1,5 +1,8 @@
-students = ["Harsh", "Shreekant", "Rushikesh"]
+students = ["Harsh", "Shreekant", "Rushikesh", "Sejal", "Pooja"]
 
-print(students[0])
-print(students[1])
-print(students[2])
+# print(students[0])
+# print(students[1])
+# print(students[2])
+
+for student in students:
+    print(student)

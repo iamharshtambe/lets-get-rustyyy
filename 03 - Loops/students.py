f4a4d47f4a78f41a2f4fb72f8e6b8e3ddd75dc3a
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-28 21:24:11 +0530

    students v4

--- 03 - Loops/students.py
+++ 03 - Loops/students.py
@@ -1,4 +1,17 @@
-students = ["Harsh", "Shreekant", "Rushikesh", "Sejal", "Pooja"]
+# lists
+students1 = ["Harsh", "Shreekant", "Rushikesh", "Sejal", "Pooja"]
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+for i in range(len(students1)):
+    print(i + 1, students1[i])
+
+# dictionaries
+students2 = {
+    "Harsh": "Tambe",
+    "Shreekant": "Belwalkar",
+    "Rushikesh": "Patil",
+    "Sejal": "Patil",
+    "Pooja": "Narkar",
+}
+
+for student in students2:
+    print(student, students2[student])

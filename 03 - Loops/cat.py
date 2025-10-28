df7ed6155e632b4bd7a1c80f207e50a700cf5ed9
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-28 19:15:42 +0530

    print meow as n times v1

--- 03 - Loops/cat.py
+++ 03 - Loops/cat.py
@@ -10,3 +10,14 @@ for _ in range(3):
     print("Meow")
 
 # print("Hello\n" * 3, end="")
+
+# printing Meow n times
+while True:
+    n = int(input("Enter n = "))
+    if n < 0:
+        continue
+    else:
+        break
+
+for i in range(n):
+    print("Meow")

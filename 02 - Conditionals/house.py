d500b1de2108ce3357710b8307422fc0684831d4
iamharshtambe <harshtambe.dev@gmail.com>
2025-10-30 15:01:40 +0530

    remove everything

--- 02 - Conditionals/house.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter name either from Tambe or Naik family: ")
-
-match name:
-    case "Harsh" | "Vijay" | "Siya":
-        print("Tambe Family")
-    case "Riya" | "Rupesh" | "Dipti":
-        print("Naik Family")
-    case _:
-        print("Who?")
